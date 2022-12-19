c552ad0ea0f4478340108b1f8030d36da9ca4abb
Andy Marks <vampwillow@gmail.com>
2022-12-19 19:43:08 +1100

    Simplest thing to make inherited test pass

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -15,13 +15,13 @@ class GildedRose(var items: Array<Item>) {
                     items[i].quality = items[i].quality + 1
 
                     if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
+                        if (items[i].sellInDays < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
+                        if (items[i].sellInDays < 6) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1
                             }
@@ -31,10 +31,10 @@ class GildedRose(var items: Array<Item>) {
             }
 
             if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
+                items[i].sellInDays = items[i].sellInDays - 1
             }
 
-            if (items[i].sellIn < 0) {
+            if (items[i].sellInDays < 0) {
                 if (items[i].name != "Aged Brie") {
                     if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
                         if (items[i].quality > 0) {
--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -6,11 +6,11 @@ import org.junit.jupiter.api.Test
 internal class GildedRoseTest {
 
     @Test
-    fun foo() {
+    fun itemNamesArePersistentAfterUpdate() {
         val items = arrayOf(Item("foo", 0, 0))
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
+        assertEquals("foo", app.items[0].name)
 
     }
 
