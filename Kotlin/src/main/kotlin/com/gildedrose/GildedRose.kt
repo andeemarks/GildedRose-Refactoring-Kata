d4dcf4b2a455fe9b4d1e5031fa033558a3496e2e
Andy Marks <vampwillow@gmail.com>
2022-12-19 20:16:27 +1100

    Magic strings/numbers -> constants

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,28 +1,36 @@
 package com.gildedrose
 
+private const val AGED_BRIE = "Aged Brie"
+private const val BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+private const val SULFURAS = "Sulfuras, Hand of Ragnaros"
+private const val ITEM_MAX_QUALITY = 50
+private const val BACKSTAGE_PASS_THRESHOLD_1 = 11
+private const val BACKSTAGE_PASS_THRESHOLD_2 = 6
+private const val ITEM_MIN_QUALITY = 0
+
 class GildedRose(var items: Array<Item>) {
 
     fun endOfDay() {
         for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+            if (items[i].name != AGED_BRIE && items[i].name != BACKSTAGE_PASSES) {
+                if (items[i].quality > ITEM_MIN_QUALITY) {
+                    if (items[i].name != SULFURAS) {
                         items[i].quality = items[i].quality - 1
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
+                if (items[i].quality < ITEM_MAX_QUALITY) {
                     items[i].quality = items[i].quality + 1
 
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellInDays < 11) {
-                            if (items[i].quality < 50) {
+                    if (items[i].name == BACKSTAGE_PASSES) {
+                        if (items[i].sellInDays < BACKSTAGE_PASS_THRESHOLD_1) {
+                            if (items[i].quality < ITEM_MAX_QUALITY) {
                                 items[i].quality = items[i].quality + 1
                             }
                         }
 
-                        if (items[i].sellInDays < 6) {
-                            if (items[i].quality < 50) {
+                        if (items[i].sellInDays < BACKSTAGE_PASS_THRESHOLD_2) {
+                            if (items[i].quality < ITEM_MAX_QUALITY) {
                                 items[i].quality = items[i].quality + 1
                             }
                         }
@@ -30,15 +38,15 @@ class GildedRose(var items: Array<Item>) {
                 }
             }
 
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+            if (items[i].name != SULFURAS) {
                 items[i].sellInDays = items[i].sellInDays - 1
             }
 
             if (items[i].sellInDays < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
+                if (items[i].name != AGED_BRIE) {
+                    if (items[i].name != BACKSTAGE_PASSES) {
+                        if (items[i].quality > ITEM_MIN_QUALITY) {
+                            if (items[i].name != SULFURAS) {
                                 items[i].quality = items[i].quality - 1
                             }
                         }
@@ -46,7 +54,7 @@ class GildedRose(var items: Array<Item>) {
                         items[i].quality = items[i].quality - items[i].quality
                     }
                 } else {
-                    if (items[i].quality < 50) {
+                    if (items[i].quality < ITEM_MAX_QUALITY) {
                         items[i].quality = items[i].quality + 1
                     }
                 }
