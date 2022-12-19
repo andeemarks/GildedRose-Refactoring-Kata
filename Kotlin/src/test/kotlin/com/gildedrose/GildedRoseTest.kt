59d00ff8bc36a5654cc4d8014139231a62663965
Andy Marks <vampwillow@gmail.com>
2022-12-19 19:55:29 +1100

    Initial set of green unit tests (missing Backstage passes reqs)

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -7,11 +7,66 @@ internal class GildedRoseTest {
 
     @Test
     fun itemNamesArePersistentAfterUpdate() {
-        val items = arrayOf(Item("foo", 0, 0))
+        val item = updateQualityOf(Item("foo", 0, 0))
+
+        assertEquals("foo", item.name)
+    }
+
+    @Test
+    fun qualityDecreasesWithEachPassingDayUpToSellInDays() {
+        val item = updateQualityOf(Item("foo", 5, 10))
+
+        assertEquals(9, item.quality)
+    }
+
+    @Test
+    fun qualityDecreasesFasterWithEachPassingDayBeyondSellInDays() {
+        val item = updateQualityOf(Item("foo", 0, 10))
+
+        assertEquals(8, item.quality)
+    }
+
+    @Test
+    fun sellInDaysDecreasesWithEachPassingDay() {
+        val item = updateQualityOf(Item("foo", 10, 0))
+
+        assertEquals(9, item.sellInDays)
+    }
+
+    @Test
+    fun agedBrieQualityIncreasesWithEachPassingDay() {
+        val item = updateQualityOf(Item("Aged Brie", 10, 5))
+
+        assertEquals(6, item.quality)
+    }
+
+    @Test
+    fun sulfurasQualityNeverDecreases() {
+        val item = updateQualityOf(Item("Sulfuras, Hand of Ragnaros", 10, 5))
+
+        assertEquals(5, item.quality)
+    }
+
+    @Test
+    fun itemQualityNeverDropsBelow0() {
+        val item = updateQualityOf(Item("foo", 10, 0))
+
+        assertEquals(0, item.quality)
+    }
+
+    @Test
+    fun itemQualityNeverExceeds50() {
+        val item = updateQualityOf(Item("Aged Brie", 10, 50))
+
+        assertEquals(50, item.quality)
+    }
+
+    private fun updateQualityOf(item: Item): Item {
+        val items = arrayOf(item)
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("foo", app.items[0].name)
 
+        return app.items[0]
     }
 
 }
